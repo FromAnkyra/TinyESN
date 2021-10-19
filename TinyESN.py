83e5c02ee6943f195b55c9703c3c3fc4ef29e224
FromAnkyra <cw1647@york.ac.uk>
2021-10-19 14:15:30 +0100

    added docstrings for existing functions

--- TinyESN.py
+++ TinyESN.py
@@ -2,7 +2,26 @@ import numpy as numpy
 import decimal
 
 class TinyESN():
-    def __init__(self, K: int, N: int, L: int, f=numpy.tanh, mode="discretised", feedback=False, topology="random", connectivity=0.1): #note: default topology is fully connected
+    """
+    ESN implementation.
+
+    class that implements ESNs with various parametres, topologies, and update styles.
+    Initialising, training, and testing the ESN are all supported.
+    currently training is only done using the pseudoinverse method.
+    """
+    def __init__(self, K: int, N: int, L: int, f=numpy.tanh, mode="discretised", feedback=False, topology="random", connectivity=0.1):
+        """
+        Initialise the ESN.
+
+        K: number of input nodes.
+        N: number of reservoir nodes.
+        L: number output nodes.
+        f (default numpy.tanh): update function
+        mode (default "discretised"): update mode. values: "discretised", "immediate".
+        feedback (default False): whether feedback is accounted for when updating.
+        topology (default random): topology of the reservoir layer. values: "random", "ring", "lattice", "torus", "fully_connected".
+        connectivity (default 0.1): connectivity of the weight matrix (only relevant for the random topology).
+        """
         #Matrix shapes are taken from [1]
         modes = ["discretised", "instantaneous"]
         if mode not in modes:
@@ -12,13 +31,15 @@ class TinyESN():
         if topology not in topologies:
             raise ValueError("please set a valid topology")
         self.topology = topology
+        if connectivity > 1 or connectivity < 0:
+            raise ValueError("please set a connectivity between 0 and 1.")
         self.connectivity = connectivity #this only comes into play if the topology is set to random
         self.feedback = feedback
         self.u = numpy.random.rand(K, 1)
         self.x = numpy.zeros((N, 1), dtype=numpy.float32)
         self.v = numpy.zeros((L, 1), dtype=numpy.float32)
         self.Wu = numpy.random.rand(N, K)
-        self.W = numpy.random.zeros((N, N), dtype=numpy.float32)
+        self.W = numpy.zeros((N, N), dtype=numpy.float32)
         self.Wv = numpy.random.rand(L, N)
         self.Wback = numpy.random.rand(N, L)
         self.func = numpy.vectorize(f)
@@ -27,34 +48,26 @@ class TinyESN():
         self.D_train = None
         self.outputs = None
         self.timestep = None
-        self.set_topology()
+        self._set_topology()
         return
 
-    def increment_timestep(self, data):
-        if self.mode == "discretised" and self.feedback == True:
-            self.increment_timestep_fb_discretised(data)
-        elif self.mode == "discretised" and self.feedback == False:
-            self.increment_timestep_no_fb_discretised(data)
-        elif self.mode == "instantaneous" and self.feedback == True:
-            self.increment_fb_inst(data)
-        else:
-            self.increment_timestep_no_fb_inst(data)
-        return
 
-    def set_topology(self):
+    def _set_topology(self):
+        """Initialise the topology accoding to what is set in init."""
         if self.topology == "random":
-            self.init_random()
+            self._init_random()
         elif self.topology == "ring":
-            self.init_ring()
+            self._init_ring()
         elif self.topology == "lattice":
-            self.init_lattice() #TODO: this is mildly broken
+            self._init_lattice() #TODO: this is mildly broken
         elif self.topology == "torus":
-            self.init_torus() #TODO: this is mildy broken
+            self._init_torus() #TODO: this is mildy broken
         elif self.topology == "fully_connected":
-            self.init_complete()
+            self._init_complete()
         return
     
-    def init_random(self):
+    def _init_random(self):
+        """Set reservoir to a random architecture with connectivity equal to that set in init."""
         #sets topology according randomly 
         size = self.x.size
         no_of_weights = int(self.connectivity * (size**2))
@@ -64,19 +77,31 @@ class TinyESN():
         self.W = placeholder
         return
     
-    '''sets the reservoir topology to one of standard ones, as described in [2]'''
-    
-    '''Question: is connectivity vs architecture: if there are two esns with the same architecture and size, are they automatically the same connectivity? Or is it a case of the architecture describes _which_ nodes can have things happen to them and then the connectivity is how many of those nodes are actually connected?'''
-    def init_ring(self):
+    def _init_ring(self):
+        """
+        Initialise the ESN with a ring topology.
+
+        From [2]:  Each node has a single self-connection and a weighted connection to each of its neighbours to its left and right.
+        
+        This imitates the topology of "delay-line reservoirs".
+        """
         placeholder = numpy.zeros((self.x.size, self.x.size))
         for i in range(self.x.size):
             coordinates = [(i, i, i), ((i-1)%self.x.size, i, (i+1)%self.x.size)]
             placeholder[tuple(coordinates)] = 1
         self.W = placeholder
-        # "Each node has a single self-connection and a weighted connection to each of its neighbours to its left and right" ([2])
         return
     
-    def init_lattice(self, rows):
+    def _init_lattice(self):
+        """
+        Initialise the ESN with a lattice topology. 
+
+        For this to work, the number of nodes N must be a square number, or else the function will return an error. 
+
+        From [2]: With this topology, we define a square grid of neurons each connected to its nearest neighbours (using its Moore neighbourhood, as commonly used in cellular automata). Each non-perimetre node has eight connections and one self-connection, resulting with each node having a maximum of nine adaptable weights in W.
+
+        This topology aims to imitate the layout of physical materia.
+        """
         #TODO: test
         if self.x.size % rows == 0:
             raise ValueError("this would not let you form a grid")
@@ -94,10 +119,16 @@ class TinyESN():
             placeholder[tuple(coordinates)] = 1
         
         self.W = placeholder
-        # "With this topology, we define a square grid of neurons each connected to its nearest neighbours (using its Moore neighbourhood, as commonly used in cellular automata). Each non-perimetre node has eight connections and one self-connection, resulting with each node having a maximum of nine adaptable weights in W" ([2])
         return
 
-    def init_torus(self, rows):
+    def _init_torus(self):
+        """
+        Initialise the ESN with a lattice topology.
+
+        For this to work, the number of nodes N must be a square number, or else the function will return an error. 
+
+        From [2]: The torus topology is a special case of the latice where the perimetre nodes are connected to give periodic boundary conditions. Each node has nine adaptable weights in W.
+        """
         #TODO: test
         if self.x.size % rows == 0:
             raise ValueError("this would not let you form a grid")
@@ -114,70 +145,83 @@ class TinyESN():
             placeholder[tuple(coordinates)] = 1
         
         self.W = placeholder
-        #"The torus topology is a special case of the latice where the perimetre nodes are connected to give periodic boundary conditions. Each node has nine adaptable weights in W" ([2])
         return
 
-    def init_complete(self):
+    def _init_complete(self):
+        """Initialise an ESN with a fully-connected topology."""
         placeholder = numpy.ones_like(self.W)
         self.W = placeholder
         return
-    
-    def init_sparse(self, connectivity: decimal.Decimal):
-        return
 
-    '''Checks to see whether the ESN has the Echo State property [1]'''
     def has_echo_state(self):
+        """Look at the values of the spectral radius and largest singular value to guess if the ESN has the echo state property[1]."""
         #calculate spectral radius
         #check the "largest singular value"
         return False
 
-    '''updates the reservoir according  to the equations laid out in [1]'''
-    def increment_timestep_no_fb_inst(self, input):
-        self.u = input
+    def _increment_timestep(self, data):
+        """Increment timestep according to the protocol chosen in init."""
+        if self.mode == "discretised" and self.feedback is True:
+            self._increment_timestep_fb_discretised(data)
+        elif self.mode == "discretised" and self.feedback is False:
+            self._increment_timestep_no_fb_discretised(data)
+        elif self.mode == "instantaneous" and self.feedback is True:
+            self._increment_fb_inst(data)
+        else:
+            self._increment_timestep_no_fb_inst(data)
+        return
+
+    def _increment_timestep_no_fb_inst(self, data):
+        """Update the ESN state and outputs according to the instantaneous equation defined in [1]. Does not take feedback into account."""
+        self.u = data
         self.x = self.func(numpy.dot(self.W, self.x) + numpy.dot(self.Wu, self.u)) 
         self.v = self.func(numpy.dot(self.Wv, self.x))  
         return
     
-    def increment_fb_inst(self, input):
-        self.u = input
+    def _increment_fb_inst(self, data):
+        """Update the ESN state and outputs according to the instantaneous equation defined in [1]. Takes feedback into account."""
+        self.u = data
         self.x = self.func(numpy.dot(self.W, self.x) + numpy.dot(self.Wu, self.u) + numpy.dot(self.Wback, self.v))
         self.v = self.func(numpy.dot(self.x, self.Wv))  
     
-    '''updates the reservoir state and outputs according to the non-instantaneous equation described in [3], adapted from the initial definition from [1]'''
-    
-    def increment_timestep_no_fb_discretised(self, input):
-        #currently using discretised ESN equations for a physical system (Stepney, 2021)
+    def _increment_timestep_no_fb_discretised(self, data):
+        """Update the ESN state and ouputs according to the discretised equation described in [3]. Does not take feedback into account."""
         new_x = self.func(numpy.dot(self.W, self.x) + numpy.dot(self.Wu, self.u)) 
         # print(f"Wv: {self.Wv.shape},\nx: {self.x.shape}")
         self.v = self.func(numpy.dot(self.Wv, self.x))  
         self.x = new_x
-        self.u = input
+        self.u = data
         self.t += 1
         return
 
-    def increment_timestep_fb_discretised(self, input):
+    def _increment_timestep_fb_discretised(self, data):
+        """Update the ESN state and outputs according to the discretised equation described in [3]. Takes feedback into account."""
         # incorporates feedback of the output
         new_x = self.func(numpy.dot(self.W, self.x) + numpy.dot(self.Wu, self.u) + numpy.dot(self.Wback, self.v))
-        self.v = self.func(numpy.dot(self.x, self.Wv))  
+        self.v = self.func(numpy.dot(self.x, self.Wv))
         self.x = new_x
-        self.u = input
+        self.u = data
         self.t += 1
         return
-    
+
     def pretty_print(self):
+        """Pretty print the reservoir."""
         print(self.u)
         print(self.x)
         print(self.v)
         return
 
-
-    '''trains the matrix using the matrix pseudoinverse method described in [4]
-    
-    Note: [4] describes this method as "it is expensive memory-wise for large design matrices [self.x]". That being said, given the scale of this ESN, it is perhaps the most straightforward option, as it is very simple to understand.'''
     def train_pseudoinverse(self, training_set):
+        """
+        Train the Output Weight matrix using the pseudo-inverse method descirbed in [4].
+
+        training_set: Dict of the training set, of form {input: target output}.
+        
+        Note: [4] describes this method as "it is expensive memory-wise for large design matrices [self.x]". That being said, given the scale of this ESN, it is perhaps the most straightforward option, as it is very simple to understand.
+        """
         #TODO: turn M into the correct shape
         for data in training_set:
-            self.increment_timestep(data)
+            self._increment_timestep(data)
             if self.t <= 10:
                 pass
             elif self.M_train is None:
@@ -198,23 +242,23 @@ class TinyESN():
                 self.Wv = numpy.transpose(numpy.dot(numpy.linalg.pinv(self.M_train), self.D_train)) #Note: the output matrix derived with this method gives a transposition of the weight matrix described in [1], hence the transposition here
         return
 
-    '''Trains the ESN using linear regression using the method described in [4].'''
-
-    # def train_linear_regression(self, training_set, regularisation_coefficient):
-    #     return
-    
     '''tests the ESN '''
     def test(self, testing_set):
-        self.increment_timestep(list(testing_set.keys())[0])
+        """
+        Test the ESN.
+
+        testing_set: Dict of testing values of form {input: target_output}.
+        """
+        self._increment_timestep(list(testing_set.keys())[0])
         self.outputs = self.v
         for data in list(testing_set.keys())[1:]:
-            self.increment_timestep(data)
+            self._increment_timestep(data)
             self.outputs = numpy.vstack((self.outputs, self.v))
         return 
 
 
 
-'''
+"""
 Bibliography:
 
 [1] Jaeger, Herbert. n.d. “The ‘echo State’ Approach to Analysing and Training Recurrent Neural Networks – with an Erratum note1.” Accessed October 5, 2021. http://www.faculty.jacobs-university.de/hjaeger/pubs/EchoStatesTechRep.pdf.
@@ -224,10 +268,4 @@ Bibliography:
 [3] Stepney, Susan. n.d. “Non-Instantaneous Information Transfer in Physical Reservoir Computing.
 
 [4] Montavon, Grégoire, Geneviève B. Orr, and Klaus-Robert Müller, eds. 2012. Neural Networks: Tricks of the Trade: Second Edition. Springer, Berlin, Heidelberg.
-'''
-            
-
-
-
-
-
+"""
