eb1a68ca56f3d8dd11b36dc059a5f4fc3e1ba162
FromAnkyra <cw1647@york.ac.uk>
2021-10-19 14:39:10 +0100

    added docstrings to NARMA10

--- NARMA10.py
+++ NARMA10.py
@@ -1,8 +1,14 @@
-"""tests NARMA10 on the TinyESN"""
+"""Creat a NARMA10 dataset"""
 import numpy
 
 class Narma10:
+    """Create a NARMA10 dataset."""
+
     def __init__(self, mode="discretised"):
+        """
+        Initialise the dataset and set the mode.
+        mode (default: "discretised"): whether the output will be acquired in an instantaneous or dicretised fashion.
+        """
         self.y = 0
         self.timestep = 0
         self.history = {}
@@ -12,15 +18,17 @@ class Narma10:
             raise ValueError("mode type is invalid.")
         self.mode = mode
         return
-    
+        
     def increment_timestep(self, current_input):
+        """Increment the timestep based on the mode chosen."""
         if self.mode == "discretised":
-            self.increment_timestep_discretised(current_input)
+            self._increment_timestep_discretised(current_input)
         else:
-            self.increment_timestep_inst(current_input)
+            self._increment_timestep_inst(current_input)
         return 
     
-    def increment_timestep_discretised(self, current_input):
+    def _increment_timestep_discretised(self, current_input):
+        """Get the output for the current input in a discretised manner."""
         history_sum = 0
         r = 0
         
@@ -40,14 +48,13 @@ class Narma10:
                 history_sum += list(self.history.values())[(self.timestep-1)-i]
 
         self.y = 0.3*self.y + (0.05*self.y)*history_sum + 1.5*input_nine*self.input + 0.1
-        
         self.input = current_input
         self.history[self.input] = float(self.y)
         self.timestep += 1
-
         return 
 
-    def increment_timestep_inst(self, current_input):
+    def _increment_timestep_inst(self, current_input):
+        """Get the output for the current input in an instantaneous manner."""
         history_sum = 0
         r = 0
         self.input = current_input
@@ -71,14 +78,18 @@ class Narma10:
         self.history[self.input] = float(self.y)
         self.timestep += 1
 
-    
-    def generate_input(self):
+    def _generate_input(self):
+        """Generate a random input."""
         return numpy.random.random_sample()/2
     
     def create_training_set_inst(self, size):
-        for i in range(size):
-            self.increment_timestep(self.generate_input())
+        """
+        Create a set of NARMA10 inputs and outputs.
         
+        size: size of the output set.
+        """
+        for _ in range(size):
+            self.increment_timestep(self._generate_input()) 
         return self.history
     
     
