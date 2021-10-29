ca7ef1f5513b38bd0dd0e7f04ba9839465b28683
FromAnkyra <cw1647@york.ac.uk>
2021-10-29 15:38:57 +0100

    made the outputs smaller

--- parity.py
+++ parity.py
@@ -1,14 +1,23 @@
+"""Module for a very simple parity benchmark."""
 from timeseries import *
 import numpy as np
 
 class Parity(TimeSeries):
+    """
+    Very simple Parity benchmark.
+    
+    inputs are integers from 0 to size-1, outputs are input%2.
+    """
+
     def __init__(self):
+        """Initialise the parity benchmark class."""
         return
 
     def create_training_set(self, size):
-        keys = [i for i in range(size)]
+        """Create the training set for the benchmark."""
+        keys = [i/size for i in range(size)]
         keys = np.random.permutation(keys)
-        values = [keys[i]%2 for i in range(size)]
+        values = [int(keys[i]*size)%2 for i in range(size)]
         training_set = {}
         for i in range(size):
             training_set[keys[i]] = values[i]
